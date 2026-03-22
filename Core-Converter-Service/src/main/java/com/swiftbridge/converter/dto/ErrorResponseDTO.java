f91d347dad51663eeb95e3c6598e88c4401aa60b
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-22 20:00:53 +0100

    feat(core-model): enforce ErrorResponseDTO validation and builder consistency

--- Core-Converter-Service/src/main/java/com/swiftbridge/converter/dto/ErrorResponseDTO.java
+++ Core-Converter-Service/src/main/java/com/swiftbridge/converter/dto/ErrorResponseDTO.java
@@ -5,19 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Objects;
 
 @Getter
-@Builder
+@Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponseDTO {
 
     @JsonProperty("errorCode")
+    @NonNull
     private String errorCode;
 
     @JsonProperty("message")
+    @NonNull
     private String message;
 
     @JsonProperty("timestamp")
     private String timestamp;
+
+    public void validate() {
+        Objects.requireNonNull(errorCode, "errorCode is required");
+        Objects.requireNonNull(message, "message is required");
+    }
 }
