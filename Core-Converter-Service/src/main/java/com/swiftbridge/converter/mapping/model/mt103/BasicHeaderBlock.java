e783a1a9a967206f2e97c97e48e8750b3a7af54f
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-22 19:57:06 +0100

    feat(core-model): add BasicHeaderBlock lombok builder and constructors

--- Core-Converter-Service/src/main/java/com/swiftbridge/converter/mapping/model/mt103/BasicHeaderBlock.java
+++ Core-Converter-Service/src/main/java/com/swiftbridge/converter/mapping/model/mt103/BasicHeaderBlock.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 import java.util.Objects;
 
 @Getter
-@Builder
-@NoArgsConstructor
+@Builder(toBuilder = true)
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 public class BasicHeaderBlock {
 
