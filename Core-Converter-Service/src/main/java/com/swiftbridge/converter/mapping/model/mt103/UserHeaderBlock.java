67e7bbbf7b729d67678762a88ee92f22cf2ad2b0
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-22 19:57:46 +0100

    feat(core-model): define UserHeaderBlock structure for optional header values

---
+++ Core-Converter-Service/src/main/java/com/swiftbridge/converter/mapping/model/mt103/UserHeaderBlock.java
@@ -0,0 +1,15 @@
+package com.swiftbridge.converter.mapping.model.mt103;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserHeaderBlock {
+
+    private String uetr;
+}
