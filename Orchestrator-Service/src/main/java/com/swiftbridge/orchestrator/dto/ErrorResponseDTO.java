8e98c5a271479c783db90c3972855e5c941b83dd
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-23 00:18:32 +0100

    feat(orchestrator): add error response dto for api error payloads

---
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/ErrorResponseDTO.java
@@ -0,0 +1,23 @@
+package com.swiftbridge.orchestrator.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseDTO {
+
+    @JsonProperty("errorCode")
+    private String errorCode;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("timestamp")
+    private String timestamp;
+}
