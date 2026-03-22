48db42fda5b73619474a2a6130e22c0313d9740a
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-22 19:56:13 +0100

    feat(core-model): enforce Mt103Message mandatory block presence constraints

--- Core-Converter-Service/src/main/java/com/swiftbridge/converter/mapping/model/mt103/Mt103Message.java
+++ Core-Converter-Service/src/main/java/com/swiftbridge/converter/mapping/model/mt103/Mt103Message.java
@@ -3,7 +3,10 @@ package com.swiftbridge.converter.mapping.model.mt103;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Getter
 @Builder
@@ -11,9 +14,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Mt103Message {
 
+    @NonNull
     private BasicHeaderBlock block1;
+    @NonNull
     private ApplicationHeaderBlock block2;
     private UserHeaderBlock block3;
+    @NonNull
     private TextBlock block4;
     private TrailerBlock block5;
+
+    public void validateRequiredBlocks() {
+        Objects.requireNonNull(block1, "block1 is required");
+        Objects.requireNonNull(block2, "block2 is required");
+        Objects.requireNonNull(block4, "block4 is required");
+    }
 }
