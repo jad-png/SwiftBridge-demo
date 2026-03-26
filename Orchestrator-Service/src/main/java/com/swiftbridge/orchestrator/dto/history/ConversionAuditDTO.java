f246cefef24146c921cf539e893f3cb121ce285f
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:37:49 +0100

    Define ConversionAuditDTO for detailed history auditing.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/history/ConversionAuditDTO.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/history/ConversionAuditDTO.java
@@ -1,11 +1,19 @@
 package com.swiftbridge.orchestrator.dto.history;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ConversionAuditDTO {
     private String transactionId;
