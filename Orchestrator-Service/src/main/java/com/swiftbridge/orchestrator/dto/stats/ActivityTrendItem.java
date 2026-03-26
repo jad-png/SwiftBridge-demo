4cf04e9494a97ccc80637b6b106abcc346c877c7
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:37:50 +0100

    Define ActivityTrendItem for dashboard statistics.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/stats/ActivityTrendItem.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/stats/ActivityTrendItem.java
@@ -1,11 +1,18 @@
 package com.swiftbridge.orchestrator.dto.stats;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ActivityTrendItem {
 
