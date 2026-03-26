7b2c26666755fc1df8fec517d1b14600444aa11f
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:37:49 +0100

    Define HistoryItemDTO for conversion history list items.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/history/HistoryItemDTO.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/history/HistoryItemDTO.java
@@ -1,11 +1,19 @@
 package com.swiftbridge.orchestrator.dto.history;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class HistoryItemDTO {
 
