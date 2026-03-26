36672df7035872f7fe98a210d899e337ba5c3fd7
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:37:50 +0100

    Define HistoryListResponse for paginated history results.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/history/HistoryListResponse.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/history/HistoryListResponse.java
@@ -1,11 +1,18 @@
 package com.swiftbridge.orchestrator.dto.history;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class HistoryListResponse {
 
@@ -13,6 +20,9 @@ public class HistoryListResponse {
     private PaginationDTO pagination;
 
     @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Builder
     public static class PaginationDTO {
         private long total;
