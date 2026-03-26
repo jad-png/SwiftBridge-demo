de0f379d568d72ecfab3c83b1e4a967e6e25a1f6
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:35:08 +0100

    Define LoginResponse DTO for authentication success.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/auth/LoginResponse.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/auth/LoginResponse.java
@@ -1,9 +1,15 @@
 package com.swiftbridge.orchestrator.dto.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class LoginResponse {
 
