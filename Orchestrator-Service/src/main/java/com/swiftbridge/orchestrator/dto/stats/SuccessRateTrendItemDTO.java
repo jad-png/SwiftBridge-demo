ab888742ab82e488e8ceeccff76f3c9ee4ecfe6b
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:39:03 +0100

    Define SuccessRateTrendItemDTO for trend analysis.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/stats/SuccessRateTrendItemDTO.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/stats/SuccessRateTrendItemDTO.java
@@ -1,11 +1,17 @@
 package com.swiftbridge.orchestrator.dto.stats;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class SuccessRateTrendItemDTO {
 
