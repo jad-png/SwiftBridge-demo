ab9d80596c4b57a360e0244c439499cb7ca4e110
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:39:53 +0100

    Update User entity with role and association details.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/entity/User.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/entity/User.java
@@ -11,14 +11,18 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "app_user")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
