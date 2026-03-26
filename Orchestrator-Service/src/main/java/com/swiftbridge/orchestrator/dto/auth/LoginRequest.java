a488038d8051ba6e1edac94f4240867fb886b394
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:34:23 +0100

    Define LoginRequest DTO for authentication.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/auth/LoginRequest.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/auth/LoginRequest.java
@@ -1,11 +1,18 @@
 package com.swiftbridge.orchestrator.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
 
     @NotBlank
