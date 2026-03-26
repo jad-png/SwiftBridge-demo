ac795d458024112d1379e54406f7904713d4c3ae
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:39:02 +0100

    Define AdminStatsResponse for admin dashboard data.

--- Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/stats/AdminStatsResponse.java
+++ Orchestrator-Service/src/main/java/com/swiftbridge/orchestrator/dto/stats/AdminStatsResponse.java
@@ -1,11 +1,18 @@
 package com.swiftbridge.orchestrator.dto.stats;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AdminStatsResponse {
 
