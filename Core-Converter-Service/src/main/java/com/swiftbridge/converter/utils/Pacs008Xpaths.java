652687289b3032ea597c14ca6f0ffebedad8e582
jad-png <j.belassiria3993@uca.ac.ma>
2026-03-27 00:31:15 +0100

    Refactor Pacs008 XPaths for increased robustness, enabling flexible extraction across various message structures and versions.

--- Core-Converter-Service/src/main/java/com/swiftbridge/converter/utils/Pacs008Xpaths.java
+++ Core-Converter-Service/src/main/java/com/swiftbridge/converter/utils/Pacs008Xpaths.java
@@ -2,49 +2,55 @@ package com.swiftbridge.converter.utils;
 
 public class Pacs008Xpaths {
 
-    public static final String REFERENCE_INSTR_ID = "//doc:CstmrCdtTrfInitn/doc:PmtInf/doc:CdtTrfTxInf/doc:PmtId/doc:InstrId";
-    public static final String REFERENCE_END_TO_END_ID = "//doc:CdtTrfTxInf/doc:PmtId/doc:EndToEndId";
-    public static final String UETR = "//doc:CdtTrfTxInf/doc:PmtId/doc:UETR";
+        public static final String REFERENCE_INSTR_ID = "//doc:PmtId/doc:InstrId";
+        public static final String REFERENCE_END_TO_END_ID = "//doc:PmtId/doc:EndToEndId";
+        public static final String UETR = "//doc:PmtId/doc:UETR";
 
-    public static final String AMOUNT_VALUE_INTERBANK = "//doc:CdtTrfTxInf/doc:IntrBkSttlmAmt";
-    public static final String AMOUNT_VALUE_INSTD = "//doc:CdtTrfTxInf/doc:InstdAmt";
-    public static final String AMOUNT_CCY_INTERBANK = "//doc:CdtTrfTxInf/doc:IntrBkSttlmAmt/@Ccy";
-    public static final String AMOUNT_CCY_INSTD = "//doc:CdtTrfTxInf/doc:InstdAmt/@Ccy";
+        public static final String AMOUNT_VALUE_INTERBANK = "//doc:IntrBkSttlmAmt";
+        public static final String AMOUNT_VALUE_INSTD = "//doc:InstdAmt";
+        public static final String AMOUNT_CCY_INTERBANK = "//doc:IntrBkSttlmAmt/@Ccy";
+        public static final String AMOUNT_CCY_INSTD = "//doc:InstdAmt/@Ccy";
 
-    public static final String SETTLEMENT_DATE_INTERBANK = "//doc:CdtTrfTxInf/doc:IntrBkSttlmDt";
-    public static final String SETTLEMENT_DATE_REQUESTED = "//doc:CdtTrfTxInf/doc:ReqdExctnDt";
+        public static final String SETTLEMENT_DATE_INTERBANK = "//doc:IntrBkSttlmDt";
+        public static final String SETTLEMENT_DATE_REQUESTED = "//doc:ReqdExctnDt";
 
-    public static final String DEBTOR_NAME = "//doc:CstmrCdtTrfInitn/doc:PmtInf/doc:Dbtr/doc:Nm";
-    public static final String DEBTOR_NAME_FALLBACK = "//doc:CdtTrfTxInf/doc:InitgPty/doc:Nm";
-    public static final String DBTR_BICFI = "//doc:CdtTrfTxInf/doc:DbtrAgt/doc:FinInstnId/doc:BICFI";
-    public static final String DBTR_BIC = "//doc:CdtTrfTxInf/doc:DbtrAgt/doc:FinInstnId/doc:Cd";
+        public static final String DEBTOR_NAME = "//doc:Dbtr/doc:Nm";
+        public static final String DEBTOR_NAME_FALLBACK = "//doc:InitgPty/doc:Nm";
+        public static final String INITIATING_PARTY_NAME = "//doc:InitgPty/doc:Nm";
+        public static final String DBTR_BICFI = "//doc:DbtrAgt//doc:BICFI";
+        public static final String DBTR_BIC = "//doc:DbtrAgt//doc:Cd";
 
-    public static final String CREDITOR_NAME = "//doc:CstmrCdtTrfInitn/doc:PmtInf/doc:CdtTrfTxInf/doc:Cdtr/doc:Nm";
-    public static final String CDTR_BICFI = "//doc:CdtTrfTxInf/doc:CdtrAgt/doc:FinInstnId/doc:BICFI";
-    public static final String CDTR_BIC = "//doc:CdtTrfTxInf/doc:CdtrAgt/doc:FinInstnId/doc:Cd";
+        public static final String CREDITOR_NAME = "//doc:Cdtr/doc:Nm";
+        public static final String CDTR_BICFI = "//doc:CdtrAgt//doc:BICFI";
+        public static final String CDTR_BIC = "//doc:CdtrAgt//doc:Cd";
 
-    public static final String CHARGE_BEARER_PMTINF = "//doc:PmtInf/doc:ChrgBr";
-    public static final String CHARGE_BEARER_TX = "//doc:CdtTrfTxInf/doc:ChrgBr";
+        public static final String CHARGE_BEARER_PMTINF = "//doc:PmtInf/doc:ChrgBr";
+        public static final String CHARGE_BEARER_TX = "//doc:CdtTrfTxInf/doc:ChrgBr";
 
-    public static final String DEBTOR_ADDRESS_ROOT = "//doc:CdtTrfTxInf/doc:Dbtr/doc:PstlAdr";
-    public static final String CREDITOR_ADDRESS_ROOT = "//doc:CdtTrfTxInf/doc:Cdtr/doc:PstlAdr";
+        public static final String DEBTOR_ADDRESS_ROOT = "//doc:Dbtr/doc:PstlAdr";
+        public static final String CREDITOR_ADDRESS_ROOT = "//doc:Cdtr/doc:PstlAdr";
 
-    public static final String[] AMOUNT_VALUE_PATHS = {
-        AMOUNT_VALUE_INSTD,
-        AMOUNT_VALUE_INTERBANK
-    };
+        public static final String[] AMOUNT_VALUE_PATHS = {
+                        AMOUNT_VALUE_INSTD,
+                        AMOUNT_VALUE_INTERBANK
+        };
 
-    public static final String[] AMOUNT_CURRENCY_PATHS = {
-        AMOUNT_CCY_INSTD,
-        AMOUNT_CCY_INTERBANK
-    };
+        public static final String[] AMOUNT_CURRENCY_PATHS = {
+                        AMOUNT_CCY_INSTD,
+                        AMOUNT_CCY_INTERBANK
+        };
 
-    public static final String[] CHARGE_BEARER_PATHS = {
-        CHARGE_BEARER_PMTINF,
-        CHARGE_BEARER_TX
-    };
+        public static final String[] UETR_PATHS = {
+                        UETR,
+                        REFERENCE_END_TO_END_ID
+        };
 
-    private Pacs008Xpaths() {
-        throw new AssertionError("Utility class should not be instantiated");
-    }
+        public static final String[] CHARGE_BEARER_PATHS = {
+                        CHARGE_BEARER_PMTINF,
+                        CHARGE_BEARER_TX
+        };
+
+        private Pacs008Xpaths() {
+                throw new AssertionError("Utility class should not be instantiated");
+        }
 }
